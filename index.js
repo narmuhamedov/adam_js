e8e7c25eec6063804f46b8d7fd3503363404f335
narmuhamedov <kingradomir@gmail.com>
2025-08-28 13:54:47 +0600

    'lesson1'

---
+++ index.js
@@ -0,0 +1,69 @@
+//Переменные и типы данных
+
+//Типы данных
+//1 String(строчный тип данных) - все что находиться в кавычках ("ffdsfsdfwf12324")
+//2 Number(числовой тип данных) - все числа
+//3 boolean(логический тип данных) - true false
+//4 underfined - неизвестный тип данных
+// var check;
+// var checkCash = 10000;
+// var withdraw_money = Number(prompt('Сколько хотите снять денег? Ваш баланс-'+ checkCash))
+
+// check = checkCash - withdraw_money
+
+// console.log("Вы сняли -- " + withdraw_money);
+
+// console.log(' Остаток -- ' + check);
+
+//null
+
+// object
+// var studentAdam = {
+//     name_ : "Ivan",
+//     age : 22,
+//     course: 4,
+//     languages: {
+//         programming: 'python',
+//         lang: 'english'
+//     }
+// }
+
+// console.log(studentAdam.languages.programming);
+
+
+
+
+//var let const
+// var userName = prompt('Как вас зовут? ')
+// console.log("Привет " + userName);
+// document.writeln("Привет " + userName);
+
+// var firstNumber = Number(prompt('введите число 1 '));
+// var secondNumber = Number(prompt('введите число 2 '));
+
+// var result = firstNumber + secondNumber;
+// console.log('Результат сложения - ' + result);
+
+
+// var userName = 'Radomir';
+// console.log(userName);
+
+
+
+
+
+
+//document.body.style.backgroundColor='red';
+
+
+
+//Вывод данных
+//document.writeln('Hello');
+//console.log('Hello Hello');
+
+
+//promt - это ввод данных в всплывашке
+// prompt('Как тебя зовут')
+
+// всплывающее окно
+//alert('Привет это первый проект на JS');
